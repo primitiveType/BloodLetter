305db7232c7bdafc2fc9ee3f83b6ce44eefc71e2
primitiveType <arthur.littlefield@gmail.com>
2020-06-18 18:43:42 -0400

    rimlighting

--- Assets/Includes/rimLight.hlsl
+++ Assets/Includes/rimLight.hlsl
@@ -1,24 +1,23 @@
-#ifndef CUSTOM_LIGHTING_INCLUDED
-#define CUSTOM_LIGHTING_INCLUDED
+#ifndef CUSTOM_RIMLIGHTING_INCLUDED
+#define CUSTOM_RIMLIGHTING_INCLUDED
 
 void AdditionalLightsAngle_float(float3 WorldNormal, float3 WorldPos, out float cosAngle){
     WorldNormal = normalize(WorldNormal);
-        cosAngle = 0;
-
-    #ifndef SHADERGRAPH_PREVIEW
+        cosAngle = 1;
+        #ifndef SHADERGRAPH_PREVIEW
 
     int pixelLightCount = GetAdditionalLightsCount();
     for (int i = 0; i < pixelLightCount; ++i)
     {
         Light light = GetAdditionalLight(i, WorldPos);
-        cosAngle += dot(WorldNormal, light.direction);
-    }
-        float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+        cosAngle = min( dot(WorldNormal, light.direction) * light.distanceAttenuation, cosAngle);
+    }   
+     float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
 
-             Light mainLight = GetMainLight(shadowCoord);
-                 cosAngle = dot(WorldNormal, mainLight.direction);
+     Light mainLight = GetMainLight(shadowCoord);
+     cosAngle = min(dot(WorldNormal, mainLight.direction) * mainLight.distanceAttenuation, cosAngle);
 
-        #endif
+     #endif
 
     
    /* Direction = mainLight.direction;
@@ -39,12 +38,12 @@ void AdditionalLightsAngle_half(half3 WorldNormal, half3 WorldPos, out half cosA
     for (int i = 0; i < pixelLightCount; ++i)
     {
         Light light = GetAdditionalLight(i, WorldPos);
-        cosAngle += dot(WorldNormal, light.direction);
+        cosAngle = min( dot(WorldNormal, light.direction), cosAngle);
     }   
-              float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+     float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
 
      Light mainLight = GetMainLight(shadowCoord);
-         cosAngle = dot(WorldNormal, mainLight.direction);
+         cosAngle = min(dot(WorldNormal, mainLight.direction), cosAngle);
 
          #endif
 
