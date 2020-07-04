5b063cd80887c8af2a2d44e6d77656a0fe1fdb9e
primitiveType <arthur.littlefield@gmail.com>
2020-07-04 16:02:15 -0400

    add harpy, pistol, shotgun. fix aggro bug, head bob

---
+++ Assets/HeadBob.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class HeadBob : MonoBehaviour
+{
+    public float walkingBobbingSpeed = 14f;
+    public float bobbingAmount = 0.05f;
+    public CharacterController controller;
+
+    float defaultPosY = 0;
+    float timer = 0;
+
+    private Transform Transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform = transform;
+
+        defaultPosY = Transform.localPosition.y; 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Abs(controller.velocity.x) > 0.1f || Mathf.Abs(controller.velocity.z) > 0.1f)
+        {
+            //Player is moving
+            timer += Time.deltaTime * walkingBobbingSpeed;
+            Transform.localPosition = new Vector3(Transform.localPosition.x, defaultPosY + Mathf.Sin(timer) * bobbingAmount, Transform.localPosition.z);
+        }
+        else
+        {
+            //Idle
+            timer = 0;
+            Transform.localPosition = new Vector3(Transform.localPosition.x, Mathf.Lerp(Transform.localPosition.y, defaultPosY, Time.deltaTime * walkingBobbingSpeed), Transform.localPosition.z);
+        }
+    }
+}
