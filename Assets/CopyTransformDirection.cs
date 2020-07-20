01cda08d15d2e3a99e399d5b1771b120cb4fd679
primitiveType <arthur.littlefield@gmail.com>
2020-07-20 19:57:54 -0400

    - prevent player from comitting suicide when looking down with hitscan weapons - fix elevator jitter. - add movement test scene - update center of l1a1 to use an elevator instead of wonky stairs.

---
+++ Assets/CopyTransformDirection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyTransformDirection : MonoBehaviour
+{
+    [SerializeField] private Transform toCopy;
+
+    private Transform myTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTransform.rotation = toCopy.rotation;
+    }
+}
