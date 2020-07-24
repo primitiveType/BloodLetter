2c18b50814c37832e5b7693f273947fc989499a9
Arthur <arthur.littlefield@gmail.com>
2020-07-23 21:23:52 -0400

    update collision logic for chainsaw, rework parts of l1a1, add timer, improve load times by keeping textures in memory. Make toughest secret a little less tough.

---
+++ Assets/KeepInMemory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepInMemory : MonoBehaviour
+{
+    public Object obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
