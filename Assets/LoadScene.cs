1eef94624a505751aa2bef20528fad0d91ab7733
Arthur <arthur.littlefield@gmail.com>
2020-09-07 09:50:54 -0400

    last changes for build

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField] private int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadSceneAsync(index);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
