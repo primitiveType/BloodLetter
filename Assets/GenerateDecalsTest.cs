7f18e20e5f7bb58ed8482ea5bed412ff42218186
Arthur <arthur.littlefield@gmail.com>
2020-09-15 18:49:31 -0400

    blood fx, fixes

---
+++ Assets/GenerateDecalsTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Bearroll.UltimateDecals;
+using UnityEngine;
+
+public class GenerateDecalsTest : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public int rows;
+
+    public int cols;
+
+    public float spacing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                var go = Instantiate(prefab, transform.position + new Vector3(i * spacing, .1f, j * spacing), transform.rotation,
+                    transform);
+                var decal = go.GetComponentInChildren<UltimateDecal>(true);
+                var random = go.GetComponentInChildren<RandomDecalAtlas>(true);
+                random.loops = true;
+                decal.atlasIndex = (i * cols) + j;
+                UD_Manager.UpdateDecal(decal);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/SpriteFromVelocity.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFromVelocity : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private List<Sprite> Up;
+    [SerializeField] private List<Sprite> Down;
+    [SerializeField] private List<Sprite> Sideways;
+    [SerializeField] private Rigidbody _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_rigidbody.velocity.normalized.y > .707)
+        {
+            spriteRenderer.sprite = Up.Random();
+        }
+        else if (_rigidbody.velocity.normalized.y < .1) //arbitrary number that feels good
+        {
+            spriteRenderer.sprite = Down.Random();
+        }
+        else
+        {
+            spriteRenderer.sprite = Sideways.Random();
+        }
+    }
+}
