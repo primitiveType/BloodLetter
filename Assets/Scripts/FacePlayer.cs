9858e147cfc8019a311fa70766bc4801af3a119a
Arthur <arthur.littlefield@gmail.com>
2020-10-25 19:50:23 -0400

    outline fixes

--- Assets/Scripts/FacePlayer.cs
+++ Assets/Scripts/FacePlayer.cs
@@ -5,6 +5,18 @@ public class FacePlayer : MonoBehaviour
 {
     [SerializeField] private Transform ToFace;
 
+    private Transform ObjectToFace
+    {
+        get
+        {
+            if (Application.isEditor && !Application.isPlaying)
+                if (Camera.current)
+                    return Camera.current.transform;
+
+            return ToFace;
+        }
+    }
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -19,17 +31,15 @@ public class FacePlayer : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if (Application.isEditor && !Application.isPlaying)
-            if (Camera.current)
-                ToFace = Camera.current.transform;
+      
 
         FaceThePlayer();
     }
 
     private void FaceThePlayer()
     {
-        if (ToFace == null) return;
-        var playerPosition = ToFace.position;
+        if (ObjectToFace == null) return;
+        var playerPosition = ObjectToFace.position;
         var position = new Vector3(playerPosition.x, transform.position.y, playerPosition.z);
         transform.LookAt(position);
     }
